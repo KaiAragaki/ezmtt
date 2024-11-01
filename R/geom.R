24ffd8d1b3b9b725a9505fbe887e193d73128aea
Kai Aragaki <kaiaragaki@proton.me>
2024-11-01 09:06:58 -0400

    add rudimentary geom_mtt and mtt_calc
    mtt_calc and geom_mtt are probably best used together. mtt_calc does all
    the stuff that mtt_fit does in terms of calculation (subtraction of
    background and normalizing to lowest condition concentration) but
    without the fitting part. geom_mtt in theory should perform the same
    fitting procedure at plot time. This is quite a bit easier to fiddle
    with to make custom plots, so despite its crudeness, it's much easier to
    work with.

---
+++ R/geom.R
@@ -0,0 +1,83 @@
+GeomMtt <- ggplot2::ggproto(
+  "GeomMtt", ggplot2::Geom,
+  required_aes = c("x", "y"),
+  draw_key = ggplot2::draw_key_path,
+  draw_group = function(data, panel_params, coord) {
+    ranges <- coord$backtransform_range(panel_params)
+    # From ggplot2's abline geom:
+    if (coord$clip == "on" && coord$is_linear()) {
+      # Ensure the line extends well outside the panel to avoid visible line
+      # ending for thick lines
+      ranges$x <- ranges$x + c(-1, 1) * diff(ranges$x)
+    }
+    # Can't have negative concentrations anyway
+    if (ranges$x[1] <= 0) ranges$x[1] <- sort(unique(data$x))[2] / 10
+    start <- ranges$x[1]
+    end <- ranges$x[2]
+    mtt_path <- create_mtt(data$x, data$y, start = start, end = end, n = 100, log = TRUE)
+    coords <- coord$transform(mtt_path, panel_params)
+    first_row <- coords[1, , drop = FALSE]
+    grid::polylineGrob(
+      coords$x, coords$y,
+      default.units = "native",
+      gp = grid::gpar(
+        col = data$colour
+      )
+    )
+  }
+)
+
+geom_mtt <- function(mapping = NULL,
+                     data = NULL,
+                     stat = "identity",
+                     position = "identity",
+                     na.rm = FALSE,
+                     show.legend = NA,
+                     inherit.aes = TRUE, ...) {
+  ggplot2::layer(
+    geom = GeomMtt, mapping = mapping, data = data, stat = stat,
+    position = position, show.legend = show.legend, inherit.aes = inherit.aes,
+    params = list(na.rm = na.rm, ...)
+  )
+}
+
+create_mtt <- function(x,
+                       y,
+                       start,
+                       end,
+                       n = 100,
+                       log = TRUE) {
+  if (log) {
+    .x <- exp(seq(log(start), log(end), length.out = n))
+  } else {
+    .x <- seq(start, end, length.out = n)
+  }
+
+  data <- data.frame(conc = x, div = y)
+  fit <- flexible_model(data)
+  eq <- get_curve_eqn(fit)
+  data.frame(x = .x, y = eq(.x))
+}
+
+flexible_model <- function(data) {
+  tryCatch(
+    return(
+      drc::drm(div ~ conc,
+        data = data, fct = drc::LL.4(),
+        lowerl = c(-Inf, 0, -Inf, -Inf),
+        upperl = c(Inf, Inf, 1, Inf)
+      )
+    ),
+    error = function(x) cli::cli_inform("Couldn't fit, relaxing parameters")
+  )
+  tryCatch(
+    return(
+      drc::drm(div ~ conc,
+        data = data,
+        fct = drc::LL.4()
+      )
+    ),
+    error = function(x) cli::cli_inform("Couldn't fit with lax params, using lm")
+  )
+  lm(div ~ conc, data = data)
+}
---
+++ R/mtt_calc.R
@@ -0,0 +1,61 @@
+#' Calculate normalized ODs without fitting
+#'
+#' It can be useful to calculate normalized ODs without fitting, particularly if
+#' you would like to use `geom_mtt`, or need to manipulate your data downstream.
+#'
+#' @param data A `data.frame` of absorbances, containing at least the `signal`,
+#'   `background`, and `dose` columns (see below)
+#' @param signal The name of the column that contains the signal absorbances
+#' @param background The name of the column that contains the background
+#'   absorbances. If NULL, will just use signal.
+#' @param dose The name of the column containing the concentrations of the drug.
+#'   Will use the lowest concentration as the baseline.
+#' @param out The name of the output column
+#' @param .by Character vector of columns to summarize by. Other columns will be
+#'   dropped
+mtt_calc <- function(data,
+                     signal = "nm562",
+                     background = "nm660",
+                     dose = "dose",
+                     out = "div",
+                     .by = NULL) {
+  required_names <- c(signal, background, dose, .by)
+  if (!all(required_names %in% names(data))) {
+    cli::cli_abort("Not all required columns are in data")
+  }
+
+  minimal <- data[, match(required_names, names(data))]
+
+  # If background is NULL, just use raw signal
+  if (is.null(background)) {
+    minimal$diff <- minimal[[signal]]
+  } else {
+    minimal$diff <- minimal[[signal]] - minimal[[background]]
+  }
+
+  group_cols <- c(dose, .by)
+  rest_cols <- setdiff(names(minimal), group_cols)
+
+  group_data <- minimal[, match(group_cols, names(minimal))]
+  rest_data <- minimal[, match(rest_cols, names(minimal))]
+  minimal$diff_mean <- ave(minimal$diff, group_data)
+  group_cols <- setdiff(group_cols, dose)
+
+  # Need to find an easy way to mutate with groups
+  # tapply or split might do the trick
+  if (length(group_cols) == 0) {
+    data <- .calc_div(minimal, dose, out)
+  } else {
+    group_data <- group_data[, match(group_cols, names(group_data))]
+    data <- tapply(minimal, group_cols, .calc_div, out = out, dose = dose) |>
+      array2DF()
+  }
+  data
+}
+
+.calc_div <- function(data, dose, out) {
+  smallest_dose_index <- which(data[[dose]] == min(data[[dose]]))[1]
+  div <- data$diff / data$diff_mean[smallest_dose_index]
+  data[[out]] <- div
+  data
+}
