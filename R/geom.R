12c6147a56adf330ef44d0b2257b099e8462f604
Kai Aragaki <kaiaragaki@proton.me>
2024-11-01 15:01:40 -0400

    allow manual model specification

--- R/geom.R
+++ R/geom.R
@@ -6,17 +6,36 @@ GeomMtt <- ggplot2::ggproto(
   draw_key = ggplot2::draw_key_path,
   draw_group = function(data, panel_params, coord) {
     ranges <- coord$backtransform_range(panel_params)
+
     # From ggplot2's abline geom:
     if (coord$clip == "on" && coord$is_linear()) {
       # Ensure the line extends well outside the panel to avoid visible line
       # ending for thick lines
       ranges$x <- ranges$x + c(-1, 1) * diff(ranges$x)
     }
+
     # Can't have negative concentrations anyway
     if (ranges$x[1] <= 0) ranges$x[1] <- sort(unique(data$x))[2] / 10
+
+    model <- unique(data$model)
+    if (length(model) > 1) {
+      cli::cli_warn(
+        "Found more than one unique model per group, using the first"
+      )
+      model <- model[1]
+    }
+
     start <- ranges$x[1]
     end <- ranges$x[2]
-    mtt_path <- create_mtt(data$x, data$y, start = start, end = end, n = 100, log = TRUE)
+    mtt_path <- create_mtt(
+      data$x,
+      data$y,
+      model,
+      start = start,
+      end = end,
+      n = 100,
+      log = TRUE
+    )
     coords <- coord$transform(mtt_path, panel_params)
     first_row <- coords[1, , drop = FALSE]
     grid::polylineGrob(
@@ -50,10 +69,13 @@ geom_mtt <- function(mapping = NULL,
 
 create_mtt <- function(x,
                        y,
+                       model,
                        start,
                        end,
                        n = 100,
                        log = TRUE) {
+  model <- select_model(model)
+
   if (log) {
     .x <- exp(seq(log(start), log(end), length.out = n))
   } else {
@@ -61,30 +83,54 @@ create_mtt <- function(x,
   }
 
   data <- data.frame(conc = x, div = y)
-  fit <- flexible_model(data)
+  fit <- model(data)
   eq <- get_curve_eqn(fit)
   data.frame(x = .x, y = eq(.x))
 }
 
 flexible_model <- function(data) {
   tryCatch(
-    return(
-      drc::drm(div ~ conc,
-        data = data, fct = drc::LL.4(),
-        lowerl = c(-Inf, 0, -Inf, -Inf),
-        upperl = c(Inf, Inf, 1, Inf)
-      )
-    ),
-    error = function(x) cli::cli_inform("Couldn't fit, relaxing parameters")
+    return(ll(data)),
+    error = \(x) cli::cli_inform("Couldn't fit, relaxing parameters")
   )
   tryCatch(
-    return(
-      drc::drm(div ~ conc,
-        data = data,
-        fct = drc::LL.4()
-      )
-    ),
-    error = function(x) cli::cli_inform("Couldn't fit with lax params, using lm")
+    return(ll_lax(data)),
+    error = \(x) cli::cli_inform("Couldn't fit with lax params, using lm")
   )
-  lm(div ~ conc, data = data)
+  lm_mtt(data)
+}
+
+ll <- function(data) {
+  drc::drm(
+    div ~ conc,
+    data = data, fct = drc::LL.4(),
+    lowerl = c(-Inf, 0, -Inf, -Inf),
+    upperl = c(Inf, Inf, 1, Inf),
+  )
+}
+
+ll_lax <- function(data) {
+  drc::drm(div ~ conc, data = data, fct = drc::LL.4())
+}
+
+lm_mtt <- function(data) {
+  stats::lm(div ~ conc, data = data)
+}
+# As for now, let's not accept any custom functions for simplicity
+select_model <- function(model) {
+  if (is.null(model) || is.na(model)) {
+    return(flexible_model)
+  }
+
+  if (model == "ll") {
+    return(ll)
+  }
+
+  if (model == "ll_lax") {
+    return(ll_lax)
+  }
+
+  if (model == "lm") {
+    return(lm_mtt)
+  }
 }
